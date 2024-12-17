a60d03550732aa2c9d75e7b92136a29538843825
haji <muhamedhajajii@outlook.com>
2024-12-17 10:36:00 +0200

    dist

--- src/app/pages/blank-layout/home/hero/hero.component.ts
+++ src/app/pages/blank-layout/home/hero/hero.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface ICarouselImagesArr {
+  url: string;
+  alt: string;
+  isActive?: boolean;
+}
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
-
+  carouselImagesArr: ICarouselImagesArr[] = [
+    {
+      url: './assets/rev/home-carousel/1.jpg',
+      alt: 'first doctor',
+      isActive: true,
+    },
+    {
+      url: './assets/rev/home-carousel/2.jpg',
+      alt: 'second doctor',
+      isActive: false,
+    },
+    {
+      url: './assets/rev/home-carousel/3.jpg',
+      alt: 'third doctor',
+      isActive: false,
+    },
+  ];
 }
