b0aa6c9995c903edeae92913b204b493eecb6061
haji <muhamedhajajii@outlook.com>
2024-12-23 05:35:44 +0200

    dist

---
+++ src/app/pages/blank-layout/contact-us/contact-us-form-ui/contact-us-form-ui.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-us-form-ui',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-us-form-ui.component.html',
+  styleUrl: './contact-us-form-ui.component.scss'
+})
+export class ContactUsFormUiComponent {
+
+}
--- src/app/pages/blank-layout/contact-us/contact-us.component.ts
+++ src/app/pages/blank-layout/contact-us/contact-us.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ContactUsFormComponent } from './contact-us-form/contact-us-form.component';
+import { ContactUsFormUiComponent } from './contact-us-form-ui/contact-us-form-ui.component';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [ContactUsFormComponent],
+  imports: [ContactUsFormComponent, ContactUsFormUiComponent],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss',
 })
