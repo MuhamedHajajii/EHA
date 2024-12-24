1a9bd35df89cbd49900a2bdf956afc0f1a9bf5fe
haji <muhamedhajajii@outlook.com>
2024-12-24 03:57:20 +0200

    dist

--- src/app/core/services/home/news-letter-data.service.ts
+++ src/app/core/services/home/news-letter-data.service.ts
@@ -1,7 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { INewsletterData } from '../../interfaces/news-letters/INewsLetterData';
 
 @Injectable({
   providedIn: 'root',
@@ -10,8 +9,8 @@ export class NewsLetterDataService {
   constructor(private _HttpClient: HttpClient) {}
 
   /** Get All News Letters Data */
-  getNewsLetterData(): Observable<INewsletterData> {
-    return <Observable<INewsletterData>>(
+  getNewsLetterData(): Observable<any> {
+    return <Observable<any>>(
       this._HttpClient.get(
         'https://digitalbondmena.com/healthcareapi/api/newsLettersData'
       )
